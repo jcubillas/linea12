c411d29a47ea5d416677bc3ac3a84fbb3162cf5e
Jose <josecubillas08@gmail.com>
2018-04-11 21:25:38 -0300

    stop with maps

--- database/migrations/2018_04_04_221649_create_branches_table.php
+++ database/migrations/2018_04_04_221649_create_branches_table.php
@@ -16,6 +16,7 @@ class CreateBranchesTable extends Migration
         Schema::create('branches', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('schedule');
             $table->timestamps();
         });
     }
--- public/js/stop.js
+++ public/js/stop.js
@@ -1,30 +1,103 @@
-window.addEventListener("load",()=> {
-    const data = {
-        branch : {},
-        stopCreate: {
-            name : "",
-            latitude: 0,
-            longitude: 0
+function initStopApp(){
+    
+        const data = {
+            branch : {},
+            stopToCreate : {
+                name :"",
+                latitude : 0,
+                longitude : 0
+            }
         }
-    }
-
-    const searchParams = new URLSearchParams(window.location.search.substring(1));
-    const branch_id = searchParams.get("branch_id");
-    function updatePage(){
-        axios.get('/brach/' + branch_id)
-        .then((resp=>data.branch = resp.data))
-        .catch((err)=>console.log(err.response.data))
-    }
-
-    //method to save
-
-    new Vue({
-        el: "#stopApp",
-        data: data,
-        methods: {
+    
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
+    
+        var bsas = {lat: -34.6037, lng: -58.3816};
+    
+    
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: bsas
+        })
+        directionsDisplay.setMap(map);
+    
+        map.addListener("click", (e) => {
+            const latLng = e.latLng
+            data.stopToCreate.latitude = latLng.lat()
+            data.stopToCreate.longitude = latLng.lng()
+        })
+    
+        function updateMarkers (stops){
+    
+            const points = stops.map( s => ({lat:s.latitude,lng:s.longitude, id : s.id}))
+    
+            points.forEach( p => {    
+                const marker = new google.maps.Marker({
+                    position: p,
+                    map: map,
+                    draggable: true,
+                    label: "" + p.id
+                })
+                marker.addListener("dragend",()=> {
+                    axios.put(`/stop/${p.id}`,{latitude:marker.position.lat() , longitude: marker.position.lng()})
+                        .then( r => updatePage() )
+                        .catch(error => console.error(error.response ? error.response.data : error))
+                })
+            })
+    
+            const waypoints =  points.slice(1, -1).map( p => ({ location : p , stopover : false}))
+    
+            directionsService.route({
+                origin: points[0],
+                destination: points[points.length - 1],
+                waypoints: waypoints,
+                optimizeWaypoints: true,
+                travelMode: 'DRIVING'
+              },function(response, status) {
+                if (status === 'OK') {
+                  directionsDisplay.setDirections(response);
+                } else {
+                    console.error(response);
+                }
+            })
+    
+            stopsPath.setMap(map);
         }
-    })
-
-    updatePage()
-
-})
+    
+        const searchParams = new URLSearchParams(window.location.search.substring(1));
+        const branchId = searchParams.get("branch_id")
+        function updatePage(){
+            axios.get(`/branch/${branchId}`)
+                .then(resp => { 
+                    data.branch = resp.data
+                    updateMarkers(data.branch.stops)
+                })
+                .catch(error => console.error(error.response ? error.response.data : error))
+        }
+    
+        function saveStop (stop){
+            stop.branchId = branchId
+            axios.post("/stop",stop)
+                .then(resp => { 
+                    data.stopToCreate =  {
+                        name :"",
+                        latitude : 0,
+                        longitude : 0
+                    }
+                    updatePage()
+                })
+                .catch(error => console.error(error.response.data))
+        } 
+    
+       new Vue({
+            el: "#appStops",
+            data : data,
+            methods : {saveStop}
+        })
+    
+        updatePage()
+    
+        
+    
+       
+    }
